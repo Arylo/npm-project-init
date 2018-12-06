6c27e92827cbec7d5762ebb654ff08f05d89dcc2
AryloYeung <arylo.open@gmail.com>
2018-12-06 12:43:25 +0800

    fix(update): fix diff versions order

--- lib/patches/index.ts
+++ lib/patches/index.ts
@@ -18,29 +18,45 @@ export const getVersion = (ver: string): IVersionObject => {
     return require(`./${ver}`);
 };
 
-export const hisVersions = fs.readdirSync(__dirname)
+export const hisVersions = fs
+    .readdirSync(__dirname)
     .filter((name) => /\d+\.\d+\.\d+\.[jt]s$/.test(name))
     .map((name) => name.replace(/\.[jt]s$/, ""));
 
 export const diffVersions = (ver: string) => {
     if (ver === constants.version) {
-        return [ ];
+        return [];
     }
     const curVersion = parseVersion(ver);
-    return hisVersions.reduce((arr, v) => {
-        arr.push(parseVersion(v));
-        return arr;
-    }, [ ] as IVersion[]).filter((v) => {
-        for (const level of ["major", "minor", "patch"]) {
-            if (v[level] > curVersion[level]) {
-                return true;
+    return hisVersions
+        .reduce(
+            (arr, v) => {
+                arr.push(parseVersion(v));
+                return arr;
+            },
+            [] as IVersion[]
+        )
+        .sort((a, b) => {
+            for (const level of ["major", "minor", "patch"]) {
+                if (a[level] === b[level]) {
+                    continue;
+                }
+                return a[level] - b[level];
             }
-            if (v[level] < curVersion[level]) {
-                return false;
+            return 0;
+        })
+        .filter((v) => {
+            for (const level of ["major", "minor", "patch"]) {
+                if (v[level] > curVersion[level]) {
+                    return true;
+                }
+                if (v[level] < curVersion[level]) {
+                    return false;
+                }
             }
-        }
-        return false;
-    }).map((v) => `${v.major}.${v.minor}.${v.patch}`);
+            return false;
+        })
+        .map((v) => `${v.major}.${v.minor}.${v.patch}`);
 };
 
 const parseVersion = (ver: string) => {
